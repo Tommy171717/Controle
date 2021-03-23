ead0c18629bd33fdb2d6c1b79475e8b92caaf5c7
tommy1717 <tommy1717@hotmail.fr>
2021-03-23 10:57:55 +0000

    rendu

--- c16_03/main.py
+++ c16_03/main.py
@@ -20,7 +20,7 @@ moyenne_eleve1math = (notes[0][2]+notes[2][2]+notes[5][2])/3
 print(moyenne_eleve1math)
 
 #4ac
-def moyenne_tuples(notes,eleve,matiere):
+def moyenne_tuple(notes,eleve,matiere):
   s=0
   i = 0
   for x in notes:
@@ -30,7 +30,7 @@ def moyenne_tuples(notes,eleve,matiere):
   moy = s / i 
   return moy
 
-print(moyenne_tuples(notes,'eleve1','math'))
+print(moyenne_tuple(notes,'eleve1','math'))
 
 #Que se passe-t-il si on fait une faute de frappe dans la saisie d'une matière ?
 # Le shell ne trouve pas la matière et signale une erreur de syntaxe
@@ -46,18 +46,77 @@ print(moyenne_tuples(notes,'eleve1','math'))
 
 #Qst 5 
 class Note:
+  instances = []  
   def __init__(self, eleve, matiere, valeur): #La méthode pour créer un objet
     self.eleve = eleve
     self.matiere = matiere
     self.valeur = valeur
 
+    Note.instances.append(self)
 
-  def afficher(self):
-    print('eleve', self.eleve, 'matiere', self.matiere, 'note', self.valeur)
+  def __str__(self):
+    return f"{self.__class__.__name__}('{self.eleve}','{self.matiere}','{self.valeur}')"
 
 
-onote = Note('eleve1', 'maths', 13)
-print(onote.eleve)
-print(onote.matiere)
-print(onote.valeur)
-Note.afficher(onote)
+  @classmethod #attention !
+  def vider(cls):
+    cls.instances = []
+
+  @classmethod #attention !
+  def moyenne(cls):
+    return(sum(n.valeur for n in cls.instances)/len(cls.instances))
+
+
+
+
+
+
+if __name__ == '__main__':
+    note1 = ('eleve1', 'math', 13)
+    note2 = ('eleve1', 'physique', 15)
+    note3 = ('eleve1', 'math', 13)
+    note4 = ('eleve1', 'eco', 12)
+    note5 = ('eleve1', 'eco', 13)
+    note6 = ('eleve1', 'math', 12)
+    note7 = ('eleve2', 'math', 13)
+    note8 = ('eleve2', 'math', 14)
+
+
+    notes = [note1, note2, note3, note4, note5, note6,note7,note8]
+
+    notes_elv1 = [note for note in notes if note[0] == 'eleve1']
+
+    print(notes_elv1)
+
+    print(sum(note[2] for note in notes_elv1)/len(notes_elv1))
+
+    notes_elv1_math = [n for n in notes_elv1 if n[1] == 'math']
+
+    print(sum(n[2] for n in notes_elv1_math)/len(notes_elv1_math))
+
+
+    print(moyenne_tuple(notes, 'eleve1', 'math'))
+
+
+    onote = Note('eleve1', 'maths', 13)
+
+
+
+
+
+    onotes = [Note(eleve, matiere,  valeur) for eleve, matiere, valeur in notes]
+
+
+
+
+    onotes = [Note(*note) for note in notes]
+
+
+
+    Note.vider()
+
+
+
+    onotes = [Note(*note) for note in notes]
+
+    print(Note.moyenne())
